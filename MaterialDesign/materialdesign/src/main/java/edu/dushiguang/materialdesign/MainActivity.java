e728ab5720a9d651917ad7437c96731fe0255441
dushiguang <dushiguang@qq.com>
2015-12-10 19:16:30 +0800

    新建MaterialDesign项目

---
+++ MaterialDesign/materialdesign/src/main/java/edu/dushiguang/materialdesign/MainActivity.java
@@ -0,0 +1,89 @@
+package edu.dushiguang.materialdesign;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
+        actionBarDrawerToggle.syncState();//初始化
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //设置导航栏NavigationView的点击事件
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_one:
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentOne()).commit();
+                        toolbar.setTitle("我的动态");
+                        break;
+                    case R.id.menu_two:
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentOne()).commit();
+                        toolbar.setTitle("我的留言");
+                        break;
+                    case R.id.menu_three:
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentOne()).commit();
+                        toolbar.setTitle("附近的人");
+                        break;
+                }
+                menuItem.setChecked(true);//点击了把它设为选中状态
+                mDrawerLayout.closeDrawers();//关闭抽屉
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
